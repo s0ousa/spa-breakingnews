86d7154011db3b8d829c01b9bb2561998e7fdaaa
Luis Sousa <las.sousa@outlook.com>
2024-03-06 18:08:50 -0300

    create data mock

---
+++ src/datas.js
@@ -0,0 +1,29 @@
+export const news = [
+    {
+        title: 'Noticia 1',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Duis massa est, vehicula ultricies turpis eget.',
+        image: 'https://ogimg.infoglobo.com.br/in/13664035-144-725/FT1086A/760/lorem-ipsum-logo.jpg',
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: 'Noticia 2',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Duis massa est, vehicula ultricies turpis eget.',
+        image: 'https://ogimg.infoglobo.com.br/in/13664035-144-725/FT1086A/760/lorem-ipsum-logo.jpg',
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: 'Noticia 3',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Duis massa est, vehicula ultricies turpis eget.',
+        image: 'https://ogimg.infoglobo.com.br/in/13664035-144-725/FT1086A/760/lorem-ipsum-logo.jpg',
+        likes: 2,
+        comments: 4
+    }, {
+        title: 'Noticia 4',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Duis massa est, vehicula ultricies turpis eget.',
+        image: 'https://ogimg.infoglobo.com.br/in/13664035-144-725/FT1086A/760/lorem-ipsum-logo.jpg',
+        likes: 2,
+        comments: 4
+    }
+]
